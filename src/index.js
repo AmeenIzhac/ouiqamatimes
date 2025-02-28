7c0783a3bfab5ac67a0c1178cac2e90fd0049855
Ameen Izhac <amehac@CSL16167.local>
2025-02-28 16:21:29 +0000

    dw

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import posthog from 'posthog-js'
+import { Analytics } from '@vercel/analytics/react';
 
 // Initialize PostHog
 posthog.init(process.env.REACT_APP_POSTHOG_KEY, {
@@ -17,6 +18,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
