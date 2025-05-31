b3aa72323bea2e06a7bcdcbf022e95cd38e8d33a
Ameen Izhac <amehac@CSL16167.local>
2025-05-31 21:10:59 +0100

    dw

---
+++ make_iqama_times.py
@@ -0,0 +1,83 @@
+# round up to nearest 15 minutes
+def fajr_adjustment(fajr_time):
+    hours, minutes = map(int, fajr_time.split(':'))
+    total_minutes = hours * 60 + minutes
+    rounded_minutes = ((total_minutes // 15) * 15) + 15
+    return f"{rounded_minutes // 60:02d}:{rounded_minutes % 60:02d}"
+
+# take away 4 mins then if before 12:30 return 12:30 else return 13:30
+def dhuhr_adjustment(dhuhr_time):
+    hours, minutes = map(int, dhuhr_time.split(':'))
+    total_minutes = hours * 60 + minutes - 4
+    rounded_minutes = ((total_minutes // 15) * 15) + 15
+    dhuhr_time = f"{rounded_minutes // 60:02d}:{rounded_minutes % 60:02d}"
+    if dhuhr_time < "12:30":
+        return "12:30"
+    else:
+        return "13:30"
+
+# add 5 minutes then round up to nearest 15 minutes
+def asr_adjustment(asr_time):
+    hours, minutes = map(int, asr_time.split(':'))
+    total_minutes = hours * 60 + minutes + 5
+    rounded_minutes = ((total_minutes // 15) * 15) + 15
+    return f"{rounded_minutes // 60:02d}:{rounded_minutes % 60:02d}"
+
+# take away 2 minutes
+def maghrib_adjustment(maghrib_time):
+    hours, minutes = map(int, maghrib_time.split(':'))
+    total_minutes = hours * 60 + minutes - 2
+    return f"{total_minutes // 60:02d}:{total_minutes % 60:02d}"
+
+# add 10 minutes
+def isha_adjustment(isha_time):
+    hours, minutes = map(int, isha_time.split(':'))
+    total_minutes = hours * 60 + minutes + 10
+    return f"{total_minutes // 60:02d}:{total_minutes % 60:02d}"
+
+
+
+
+
+# open the file and check format good
+with open("athan_times_2025.txt", "r") as file:
+    lines = file.readlines()
+    if not lines:
+        raise ValueError("rekt1")
+    if len(lines[0].split()) != 10:
+        raise ValueError("rekt2")
+    if lines[0].split()[2] != "Fri":
+        print(lines[0][2])
+        raise ValueError("rekt3")
+
+new_times = []
+
+parts = lines[0].split()
+fajr_time = parts[3]
+dhuhr_time = parts[5]
+asr_time = parts[7]
+isha_time = parts[9]
+
+for line in lines:
+    parts = line.split()
+    if parts[2] == "Fri":
+        fajr_time = fajr_adjustment(parts[3])
+        dhuhr_time = dhuhr_adjustment(parts[5])
+        asr_time = asr_adjustment(parts[7])
+
+    maghrib_time = maghrib_adjustment(parts[8])
+    isha_time = isha_adjustment(parts[9])
+    
+    parts[3] = fajr_time
+    parts[5] = dhuhr_time
+    parts[7] = asr_time
+    parts[8] = maghrib_time
+    parts[9] = isha_time
+    
+    new_times.append("\t".join(parts))
+
+with open("iqama_times_2025.txt", "w") as output_file:
+    for line in new_times:
+        output_file.write(line + "\n")
+
+
---
+++ make_month_csv.py
@@ -0,0 +1,25 @@
+import csv
+
+def get_month_times(month): 
+    with open("athan_times_2025.txt", "r") as file:
+        athan_lines = file.readlines()
+    with open("iqama_times_2025.txt", "r") as file:
+        iqama_lines = file.readlines()
+
+    with open(f"{month.lower()}_prayer_times_2025.csv", "w", newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        writer.writerow([month.lower(), "weekday", "fajr", "sunrise", "zuhr", "asr1", "asr2", "maghrib", "isha", "", "fajr", "zuhr", "asr", "maghrib", "isha"])
+
+        month_athan_lines = [line for line in athan_lines if line.split()[0].lower() == month.lower()]
+        month_iqama_lines = [line for line in iqama_lines if line.split()[0].lower() == month.lower()]
+
+        for i in range(len(month_athan_lines)):
+            athan_times = month_athan_lines[i].split()
+            iqama_times = month_iqama_lines[i].split()
+            if athan_times[2] == "Fri":
+                writer.writerow([str(int(athan_times[1])), athan_times[2], athan_times[3], athan_times[4], athan_times[5], athan_times[6], athan_times[7], athan_times[8], athan_times[9], "", iqama_times[3], iqama_times[5], iqama_times[7], iqama_times[8], iqama_times[9]])
+            else:
+                writer.writerow([str(int(athan_times[1])), athan_times[2], athan_times[3], athan_times[4], athan_times[5], athan_times[6], athan_times[7], athan_times[8], athan_times[9], "", "", "", "", iqama_times[8], ""])
+
+get_month_times("jun")
